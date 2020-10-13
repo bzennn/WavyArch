5942c0159a0c36fe9e332c5703c2f5fcd85534de
bzennn <bzennn@192.168.56.1>
2020-10-13 23:18:42 +0300

    Updated WebContextConfig
    Added:
    	* Static resource handler

--- WavyArch/src/main/java/xyz/bzennn/wavyarch/config/WebContextConfig.java
+++ WavyArch/src/main/java/xyz/bzennn/wavyarch/config/WebContextConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
@@ -27,5 +29,12 @@ public class WebContextConfig implements WebMvcConfigurer {
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+	      .addResourceHandler("/resources/**")
+	      .addResourceLocations("/WEB-INF/resources/");
 	}
 }
