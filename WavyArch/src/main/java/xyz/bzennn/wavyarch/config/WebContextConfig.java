654d9b22f15f98fed054eb5da3d6f5020274a49e
bzennn <bzennn@bzennn-pc.mshome.net>
2020-10-14 22:53:53 +0300

    Refactored WebContextConfig class

--- WavyArch/src/main/java/xyz/bzennn/wavyarch/config/WebContextConfig.java
+++ WavyArch/src/main/java/xyz/bzennn/wavyarch/config/WebContextConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.DefaultServletHandlerCo
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.resource.PathResourceResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
