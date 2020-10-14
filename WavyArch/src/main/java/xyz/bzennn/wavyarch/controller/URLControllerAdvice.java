73249669ab5005b26deb0909074bd7c226dd6806
bzennn <bzennn@bzennn-pc.mshome.net>
2020-10-14 22:48:36 +0300

    Added URL Advice for advising request URL, URI and viewName

---
+++ WavyArch/src/main/java/xyz/bzennn/wavyarch/controller/URLControllerAdvice.java
@@ -0,0 +1,33 @@
+package xyz.bzennn.wavyarch.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class URLControllerAdvice {
+
+	@ModelAttribute
+	public void addURIAttribute(HttpServletRequest request, Model model) {
+		model.addAttribute("uri", request.getRequestURI());
+	}
+	
+	@ModelAttribute
+	public void addURLAttribute(HttpServletRequest request, Model model) {
+		model.addAttribute("url", request.getRequestURL());
+	}
+	
+	@ModelAttribute
+	public void addViewNameAttribute(HttpServletRequest request, Model model) {
+		String[] uriParts = request.getRequestURI().replaceFirst("/WavyArch/", "").split("/");
+		
+		String viewName = "";
+		if (uriParts.length > 0) {
+			viewName = uriParts[0];
+		}
+		
+		model.addAttribute("viewName", viewName);
+	}
+}
