312d630f16e9c9b1c8a3dcbbdb33876bdbd59f29
bzennn <dima370794@gmail.com>
2020-10-27 22:23:04 +0300

    Added SignUpController class (WIP)

--- WavyArch/src/main/java/xyz/bzennn/wavyarch/controller/SignUpController.java
+++ WavyArch/src/main/java/xyz/bzennn/wavyarch/controller/SignUpController.java
@@ -1,8 +1,17 @@
 package xyz.bzennn.wavyarch.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import xyz.bzennn.wavyarch.data.model.Account;
+import xyz.bzennn.wavyarch.form.UserSignUpForm;
+import xyz.bzennn.wavyarch.service.AccountService;
 
 /**
  * User Sign Up controller
@@ -14,13 +23,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("signup")
 public class SignUpController {
 
+	@Autowired
+	AccountService accountService;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public String showSignUpPage() {
 		return "signup";
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public String handleSignUp(String login, String password, String passwordRepeat) {
+	public String handleSignUp(@Valid UserSignUpForm form, Errors errors, Model model) {
+		Account account = form.buildAccount();
+		
+		if (errors.getAllErrors() != null && !errors.getAllErrors().isEmpty()) {
+			model.addAttribute("errors", errors.getAllErrors());
+			model.addAttribute("formData", form);
+			
+			return "signup";
+		}
+		
+		accountService.save(account);
 		
 		return "redirect:/";
 	}
