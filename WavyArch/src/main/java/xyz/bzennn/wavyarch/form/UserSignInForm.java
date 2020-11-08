897ae5b13cd672721607027a8f6dc755bfafc8e8
bzennn <dima370794@gmail.com>
2020-11-08 21:41:40 +0300

    Added sign in validated form class

---
+++ WavyArch/src/main/java/xyz/bzennn/wavyarch/form/UserSignInForm.java
@@ -0,0 +1,37 @@
+package xyz.bzennn.wavyarch.form;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import xyz.bzennn.wavyarch.validation.annotation.AccountCanBeAuthenticated;
+import xyz.bzennn.wavyarch.validation.annotation.AccountExists;
+
+@AccountCanBeAuthenticated(login = "login", password = "password", message = "{field.password.wrong}")
+public class UserSignInForm {
+	
+	@NotNull(message = "{field.login.required}")
+	@NotBlank(message = "{field.login.required}")
+	@Size(min = 1, max = 20, message = "{field.login.size}")
+	@AccountExists(message = "{field.login.exists}")
+	private String login;
+	
+	@NotNull(message = "{field.password.required}")
+	@NotBlank(message = "{field.password.required}")
+	@Size(min = 1, max = 20, message = "{field.password.size}")
+	private String password;
+
+	public UserSignInForm(String login, String password) {
+		this.login = login;
+		this.password = password;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+}
