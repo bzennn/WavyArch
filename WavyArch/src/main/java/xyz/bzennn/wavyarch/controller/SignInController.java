5a7717440935e0c5b8c98f766c43425a5e4b156a
bzennn <dima370794@gmail.com>
2020-11-08 21:43:13 +0300

    Added basic authentication to SignInController

--- WavyArch/src/main/java/xyz/bzennn/wavyarch/controller/SignInController.java
+++ WavyArch/src/main/java/xyz/bzennn/wavyarch/controller/SignInController.java
@@ -1,8 +1,25 @@
 package xyz.bzennn.wavyarch.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import xyz.bzennn.wavyarch.form.UserSignInForm;
 
 /**
  * User Sign In controller
@@ -13,10 +30,40 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("signin")
 public class SignInController {
-
+	private static Logger log = LogManager.getLogger(SignInController.class);
+	
+	@Autowired
+	private AuthenticationManager authManager;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public String showSignUpPage() {
 		return "signin";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String handleSignIn(@Valid UserSignInForm form, Errors errors, Model model, HttpServletRequest request) {
+		
+		if (errors.getAllErrors() != null && !errors.getAllErrors().isEmpty()) {
+			model.addAttribute("errors", errors.getAllErrors());
+			model.addAttribute("formData", form);
+			
+			return "signin";
+		}
+		
+		UsernamePasswordAuthenticationToken authReq =
+				new UsernamePasswordAuthenticationToken(form.getLogin(), form.getPassword());
+		
+		try {
+			Authentication auth = authManager.authenticate(authReq);
+			SecurityContext securityContext = SecurityContextHolder.getContext();
+			securityContext.setAuthentication(auth);
+			HttpSession httpSession = request.getSession(true);
+			httpSession.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, securityContext);
+		} catch (Exception e) {
+			log.error("Authentication error!", e);
+		}	
+		
+		return "redirect:/";
 	}
 	
 }
