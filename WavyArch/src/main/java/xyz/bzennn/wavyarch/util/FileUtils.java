992b238abdedea4f554c770025dceccd5170f6a7
bzennn <dima370794@gmail.com>
2020-11-15 20:34:19 +0300

    Transform FileUtils class to Component

--- WavyArch/src/main/java/xyz/bzennn/wavyarch/util/FileUtils.java
+++ WavyArch/src/main/java/xyz/bzennn/wavyarch/util/FileUtils.java
@@ -3,19 +3,22 @@ package xyz.bzennn.wavyarch.util;
 import java.io.File;
 import java.util.UUID;
 
+import org.springframework.stereotype.Component;
+
 /**
  * File utility class
  *
  * @author bzennn
  * @version 1.0
  */
+@Component
 public class FileUtils {
 	
-	public static File getUniqueFile(String prefix, String suffix, File directory) {
+	public File getUniqueFile(String prefix, String suffix, File directory) {
 		return new File(directory, getUniqueFileName(prefix, suffix, directory));
 	}
 	
-	public static String getUniqueFileName(String prefix, String suffix, File directory) {
+	public String getUniqueFileName(String prefix, String suffix, File directory) {
 		String fileName = getFileName(prefix, suffix);
 		while (new File(directory, fileName).exists()) {
 			fileName = getFileName(prefix, suffix);
@@ -24,7 +27,7 @@ public class FileUtils {
 		return directory.getPath() + fileName;
 	}
 	
-	public static String getFileName(String prefix, String suffix) {
+	public String getFileName(String prefix, String suffix) {
 		StringBuffer sb = new StringBuffer(prefix);
 		sb.append("-")
 			.append(UUID.randomUUID())
