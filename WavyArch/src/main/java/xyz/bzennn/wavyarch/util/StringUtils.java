7d774a963211e3f767c40d67712b5c1152dcf71c
bzennn <dima370794@gmail.com>
2020-11-28 22:03:45 +0300

    Code clean up

--- WavyArch/src/main/java/xyz/bzennn/wavyarch/util/StringUtils.java
+++ WavyArch/src/main/java/xyz/bzennn/wavyarch/util/StringUtils.java
@@ -20,24 +20,6 @@ public class StringUtils {
 	}
 	
 	public String encodeToUtf8(String str) throws UnsupportedEncodingException {
-//		String[] partsSlash = str.split("/");
-//		
-//		StringBuilder mainBuilder = new StringBuilder();
-//		for (int i = 0; i < partsSlash.length; i++) {
-//			String[] parts = partsSlash[i].split(" ");
-//			StringBuilder sb = new StringBuilder();
-//			for (int j = 0; j < parts.length; j++) {
-//				sb.append(URLEncoder.encode(parts[j], "UTF-8"));
-//				if (j != parts.length - 1) {
-//					sb.append(" ");
-//				}
-//			}
-//			mainBuilder.append(sb);
-//			if (i != partsSlash.length - 1) {
-//				mainBuilder.append("/");
-//			}
-//		}
-		
 		StringBuilder mainBuilder = new StringBuilder();
 		for (String ch : str.split("")) {
 			if (isAsciiString(ch)) {
