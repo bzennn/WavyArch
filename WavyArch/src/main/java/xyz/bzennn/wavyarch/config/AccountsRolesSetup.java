ee77d62a64b665745a284c5106020089fb8b0273
bzennn <dima370794@gmail.com>
2020-12-06 16:24:25 +0300

    Fixed account role setup class

--- WavyArch/src/main/java/xyz/bzennn/wavyarch/config/AccountsRolesSetup.java
+++ WavyArch/src/main/java/xyz/bzennn/wavyarch/config/AccountsRolesSetup.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 
 import xyz.bzennn.wavyarch.data.dao.AccountDao;
 import xyz.bzennn.wavyarch.data.dao.AccountRoleDao;
@@ -19,8 +22,11 @@ import xyz.bzennn.wavyarch.data.model.AccountRole;
  * @author bzennn
  * @version 1.0
  */
+@PropertySource("classpath:admins.properties")
 public class AccountsRolesSetup {
 
+	private Logger logger = LogManager.getLogger(AccountsRolesSetup.class);
+	
 	@Value("${admins_list}")
 	private String adminsList;
 	
@@ -40,10 +46,12 @@ public class AccountsRolesSetup {
 			
 			List<Account> accounts = accountDao.findAll();
 			
+			logger.info("Accounts role setup!");
 			for (Account account : accounts) {
 				if (logins.contains(account.getLogin()) && !account.getRole().equals(adminRole)) {
 					account.setRole(adminRole);
 					accountDao.update(account);
+					logger.info("Added admin role to user: " + account.getLogin());
 				} else if (account.getRole().equals(adminRole)) {
 					account.setRole(userRole);
 					accountDao.update(account);
